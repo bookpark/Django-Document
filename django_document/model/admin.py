e9fe09578445dd0922346d47b76e5caef0dde034
Booki Park <bkbkgg@gmail.com>
2017-10-10 14:07:09 +0900

    admin.py에 User 등록

--- django_document/model/admin.py
+++ django_document/model/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from model.models import Manufacturer, Car
+from model.models import Manufacturer, Car, User
 
 admin.site.register(Manufacturer)
 admin.site.register(Car)
+admin.site.register(User)
