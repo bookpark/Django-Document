6cb40942d7ef49bf374b1972b72f65905a5f9c29
Booki Park <bkbkgg@gmail.com>
2017-10-10 14:07:35 +0900

    model 추가

--- django_document/model/models.py
+++ django_document/model/models.py
@@ -15,6 +15,15 @@ class Fruit(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
 
 
+# 만약 아직 정의되지 않은 ForeignKey를 정의하고 싶다면 참조하는 클래스를 문자열로 넣어준다 (문자열이기 때문에 rename 시킬 때는 같이 바뀌지 않는다
+class Car(models.Model):
+    manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE)
+    name = models.CharField(max_length=50, blank=True)
+
+    def __str__(self):
+        return f'{self.manufacturer.name} - {self.name}'
+
+
 class Manufacturer(models.Model):
     name = models.CharField(max_length=50)
 
@@ -22,9 +31,11 @@ class Manufacturer(models.Model):
         return self.name
 
 
-class Car(models.Model):
-    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
-    name = models.CharField(max_length=50, blank=True)
+# 동등한 유저일 때 관계를 가지는 경우
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    # 배우자가 없어져도 자신은 없어지지 않는다 (on_delete=models.SET_NULL)
+    spouse = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
-        return f'{self.manufacturer.name} - {self.name}'
+        return self.name
