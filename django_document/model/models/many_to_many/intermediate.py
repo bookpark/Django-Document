4a0413960983263aeeeefb0100bfaf877a2b4c9f
Booki Park <bkbkgg@gmail.com>
2017-10-11 11:54:31 +0900

    중간자 모델 수정

--- django_document/model/models/many_to_many/intermediate.py
+++ django_document/model/models/many_to_many/intermediate.py
@@ -14,18 +14,38 @@ class Idol(models.Model):
         return self.name
 
 
+# through_fields 사용
 class Group(models.Model):
     name = models.CharField(max_length=30)
     debut_date = models.DateField()
-    members = models.ManyToManyField(Idol, through='Membership')
+    members = models.ManyToManyField(
+        Idol,
+        through='Membership',
+        # 순서 중요 (source 다음 target)
+        # source의 의미 (group의 idol (O), idol의 group (X))
+        through_fields=('group', 'idol'),
+    )
 
     def __str__(self):
         return self.name
 
 
 class Membership(models.Model):
-    idol = models.ForeignKey(Idol, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    idol = models.ForeignKey(
+        Idol,
+        on_delete=models.CASCADE,
+        related_name='membership_set',
+    )
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+    )
+    recommender = models.ForeignKey(
+        Idol,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='recommend_membership_set',
+    )
     joined_date = models.DateField()
     is_active = models.BooleanField()
 
