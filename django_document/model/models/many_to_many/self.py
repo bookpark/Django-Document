a0fd45fd376d696a982c0056cdb9a45ea77985ea
Booki Park <bkbkgg@gmail.com>
2017-10-11 16:10:01 +0900

    ManyToManyField에 null=True 제거

--- django_document/model/models/many_to_many/self.py
+++ django_document/model/models/many_to_many/self.py
@@ -8,7 +8,10 @@ __all__ = (
 # 방향성이 없고 서로 대칭 관계를 갖는다 (기본값)
 class FacebookUser(models.Model):
     name = models.CharField(max_length=30)
-    friends = models.ManyToManyField('self', blank=True, null=True)
+    friends = models.ManyToManyField(
+        'self',
+        blank=True,
+    )
 
     def __str__(self):
         return self.name
