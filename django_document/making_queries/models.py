b0834995034e84ae721ffc98d20ab98f4898be2d
Booki Park <bkbkgg@gmail.com>
2017-10-13 11:57:08 +0900

    Entry 클래스에 ordering Meta 추가

--- django_document/making_queries/models.py
+++ django_document/making_queries/models.py
@@ -41,5 +41,8 @@ class Entry(models.Model):
     n_pingbacks = models.IntegerField(default=0)
     rating = models.IntegerField(default=0)
 
+    class Meta:
+        ordering = ['-pk', 'headline']
+
     def __str__(self):
         return self.headline
